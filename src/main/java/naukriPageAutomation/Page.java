e3f97cec7877593b42038fdfba3ba1236e72ac35
ThulluruChandu <thulluruchandu999@gmail.com>
2024-12-12 13:19:56 +0530

    first commit

---
+++ src/main/java/naukriPageAutomation/Page.java
@@ -0,0 +1,155 @@
+package naukriPageAutomation;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+import java.nio.file.Paths;
+import java.time.Duration;
+
+public class Page {
+
+    private WebDriver driver;
+
+    private By loginButton = By.xpath("//a[text()='Login']");
+    private By enterEmail = By.xpath("//input[@placeholder='Enter your active Email ID / Username']");
+    private By enterPassword = By.xpath("//input[@placeholder='Enter your password']");
+    private By ClickOnLogin = By.xpath("//button[text()='Login']");
+
+    private By ClickViewProfile = By.xpath("//a[text()='View & Update Profile']");
+    private By VerifyPage = By.xpath("//div[@title='Chandu Thulluru']");
+
+    private By ClickOnEditButton = By.xpath("//em[text()='editOneTheme']");
+
+    private By ClickOnLocationButton = By.xpath("//input[@placeholder='Tell us about your current location']");
+
+    private By ClickOnUpdateKeySkillsButton = By.xpath("//span[text()='Key skills']/following-sibling::span[text()='editOneTheme']");
+
+    private By ClickOnSaveButton = By.id("saveBasicDetailsBtn");
+
+    private By MainMenu = By.xpath("//div[@class='nI-gNb-drawer__icon']");
+
+    private By ClickOnSave = By.xpath("//button[text()='Save']");
+
+    private By LogOut = By.xpath("//a[@title='Logout']");
+
+    private By UserName = By.id("name");
+
+    private By ResumeHeadLine = By.xpath("//span[text()='Resume headline']/following-sibling::span[text()='editOneTheme']");
+
+    private By ResumeHeadLineTextArea = By.xpath("//textarea[@class='resumeHeadlineTxt materialize-textarea']");
+
+    public Page(WebDriver driver) {
+        this.driver = driver;
+    }
+
+
+    public void clickOnLogin() {
+        driver.findElement(loginButton).click();
+    }
+
+    public void enterEmailId() {
+        driver.findElement(enterEmail).sendKeys("thulluruchandu333@gmail.com");
+
+    }
+
+    public void enterPassword() {
+        driver.findElement(enterPassword).sendKeys("Ch@ndu9010");
+
+    }
+
+    public void clickOnLoginButton() {
+        driver.findElement(ClickOnLogin).click();
+    }
+
+    public String verifyLogin() {
+        WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(5));
+        WebElement userName = wait.until(ExpectedConditions.visibilityOfElementLocated(VerifyPage));
+
+        return driver.findElement((By) userName).getText();
+    }
+
+    public void navigateProfile() {
+        driver.findElement(MainMenu).click();
+        driver.findElement(ClickViewProfile).click();
+    }
+
+    public void clickOnEdit() {
+        driver.findElement(ClickOnEditButton).click();
+    }
+
+    public void clickOnLocationUpdate() {
+
+        driver.findElement(ClickOnSaveButton).click();
+
+    }
+
+    public void clickOnUpdateResumeHeadLine() throws InterruptedException {
+
+        driver.wait(2);
+        driver.findElement(ResumeHeadLine).click();
+        driver.findElement(ResumeHeadLineTextArea).clear();
+        driver.findElement(ResumeHeadLineTextArea).sendKeys("Software Test Automation Engineer with 3 Years of Experience in Automation Testing");
+        ;
+        driver.findElement(ClickOnSave).click();
+
+
+    }
+
+    public void clickOnSaveDetails() {
+
+        driver.findElement(ClickOnSaveButton).click();
+
+    }
+
+    public void updateResume() {
+        WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(5));
+        WebElement resumeSection = wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("//input[@value='Update resume']")));
+        resumeSection.click();
+        String resumePath = Paths.get("src/main/resources/resumeFile/Chandu.pdf").toAbsolutePath().toString();
+        resumeSection.sendKeys(resumePath);
+    }
+
+    public void updateUserName() {
+
+        driver.findElement(UserName).click();
+        driver.findElement(UserName).clear();
+        driver.findElement(UserName).sendKeys("Chandu Thulluru");
+        driver.findElement(ClickOnSave).click();
+
+
+    }
+
+    public void logOut() {
+        driver.findElement(MainMenu).click();
+        driver.findElement(LogOut).click();
+
+    }
+}
+
+
+//-------------------------------------------------------------------------------------------------------------
+
+
+//        WebElement element = driver.findElement(By.xpath("//form[@name='resumeHeadlineForm']"));
+//        driver.switchTo().frame(element);
+
+//        WebElement element = driver.findElement(ClickOnUpdateKeySkillsButton);
+//        JavascriptExecutor js = (JavascriptExecutor) driver;
+//        js.executeScript("arguments[0].click();",element);
+//        driver.findElement(ClickOnUpdateKeySkillsButton).click();
+
+
+//        driver.findElement(CurrencyUpdate).clear();
+//        driver.findElement(CurrencyUpdate).sendKeys("655000");
+
+
+//        driver.findElement(ClickOnLocationButton).sendKeys("Hyderabad, Telangana");
+//      driver.findElement(By.id("resumeHeadlineTxt")).sendKeys("Software Test Automation Engineer with 3+ Years of Experience in Automation Testing");
+
+
+//        WebDriverWait wait =new WebDriverWait(driver, Duration.ofSeconds(5));
+//        WebElement Section = wait.until(ExpectedConditions.visibilityOfElementLocated((ResumeHeadLine)));
+//        Section.click();
---
+++ src/test/java/automationTest/NaukriTest.java
@@ -0,0 +1,68 @@
+package automationTest;
+
+import naukriPageAutomation.Page;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
+
+import java.util.concurrent.TimeUnit;
+
+public class NaukriTest {
+
+    WebDriver driver;
+    private Page loginPage;
+
+    @BeforeMethod
+    public void setLoginPage() {
+        driver = new ChromeDriver();
+        driver.manage().window().maximize();
+        driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+        driver.get("https://www.naukri.com/");
+        loginPage = new Page(driver);
+    }
+
+    @Test
+    public void enterValidUsernameAndPassword() {
+        loginPage.clickOnLogin();
+        loginPage.enterEmailId();
+        loginPage.enterPassword();
+        loginPage.clickOnLoginButton();
+
+//       update basic name details
+        loginPage.navigateProfile();
+        loginPage.clickOnEdit();
+        loginPage.updateUserName();
+
+
+    }
+
+    @AfterMethod
+    public void tearDown() {
+        loginPage.logOut();
+        driver.quit();
+    }
+}
+
+
+//--------------------------------------------------------------------------
+
+//        loginPage.clickOnEdit();
+//        loginPage.clickOnSaveDetails();
+
+//        verify
+//        String expectedUserName = "Chandu Thulluru";
+//        String actualUserName = loginPage.verifyLogin();
+//        Assert.assertEquals(actualUserName, expectedUserName, "Logeed-in username does not macth");
+
+//update basic details
+//        loginPage.clickOnEdit();
+//        loginPage.clickOnLocationUpdate();
+
+//        update Key Skills
+//        loginPage.clickOnKeySkillsUpdate();
+
+//        update Resume Head line
+
+//        loginPage.clickOnUpdateResumeHeadLine();

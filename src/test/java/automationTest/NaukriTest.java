dda283f11c10935b6816143eeb66dabfc7223a56
ChanduThulluru <thulluruchandu999@gmail.com>
2025-05-15 13:47:20 +0530

    changes made by github actions

--- src/test/java/automationTest/NaukriTest.java
+++ src/test/java/automationTest/NaukriTest.java
@@ -3,7 +3,7 @@ package automationTest;
 import naukriPageAutomation.Page;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
-import org.openqa.selenium.edge.EdgeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
@@ -17,7 +17,14 @@ public class NaukriTest {
 
     @BeforeMethod
     public void setLoginPage() {
-        driver = new ChromeDriver();
+
+        ChromeOptions options = new ChromeOptions();
+        options.addArguments("--headless");
+        options.addArguments("--disable-gpu");
+        options.addArguments("--no-sandbox");
+
+        WebDriver driver = new ChromeDriver(options);
+//        driver = new ChromeDriver();
         driver.manage().window().maximize();
         driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
         driver.get("https://www.naukri.com/");
